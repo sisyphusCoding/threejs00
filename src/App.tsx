daab3dd2527ef5a11ae2dd9324ab70cc32833a33
Anish Kanna <sautronik@gmail.com>
2022-07-14 14:25:11 +0530

    done with todya

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,51 @@ import * as THREE from 'three'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { ACESFilmicToneMapping, Vector3 } from 'three'
-import {a,useSpring} from '@react-spring/three'
-import { softShadows, MeshWobbleMaterial } from '@react-three/drei'
+import { a, useSpring } from '@react-spring/three'
+import {
+  softShadows,
+  MeshWobbleMaterial,
+  Sky,
+  Backdrop,
+  MeshReflectorMaterial,
+  Cloud,
+  SpotLight,
+  MeshDistortMaterial,
+  useDepthBuffer
+} from '@react-three/drei'
 
 interface BoxProps {
   position: any
   size?: any
   color: any
-  r:number
+  r: number
 }
+softShadows({
+  frustum: 3.75,
+  size: 0.005,
+  near: 9.5,
+  samples: 17,
+  rings: 11 // Rings (default: 11) must be a int
+})
 
-export const Box: FC<BoxProps> = ({ position, size, color,r}) => {
+export const Box: FC<BoxProps> = ({ position, size, color, r }) => {
   const [expand, setExpand] = useState<boolean>(false)
   const props = useSpring({
-    scale: expand?1.5:1
+    scale: expand ? 1.5 : 1
   })
-
 
   const meshRef = useRef<THREE.Mesh>(null!)
   useFrame(
-    (state, delta) =>( 
-    meshRef.current.rotation.x = meshRef.current.rotation.y = meshRef.current.rotation.z += 0.004 * r,
-    meshRef.current.position.y = position[1] + Math[r > 0.5 ? 'cos' : 'sin'](state.clock.getElapsedTime() * r) * r
-    ))
+    (state, delta) => (
+      (meshRef.current.rotation.x =
+        meshRef.current.rotation.y =
+        meshRef.current.rotation.z +=
+          0.004 * r),
+      (meshRef.current.position.y =
+        position[1] +
+        Math[r > 0.5 ? 'cos' : 'sin'](state.clock.getElapsedTime() * r) * r)
+    )
+  )
   return (
     <a.mesh
       onClick={() => setExpand(!expand)}
@@ -37,55 +59,69 @@ export const Box: FC<BoxProps> = ({ position, size, color,r}) => {
       <boxBufferGeometry attach="geometry" args={size} />
       <MeshWobbleMaterial
         speed={1}
-        factor={0.6}
+        factor={0.7}
         attach="material"
         color={color}
       />
-      <OrbitControls />
     </a.mesh>
   )
 }
 
 const App = () => {
+  const [target] = useState(() => new THREE.Object3D())
+  const thisGroup = useRef<THREE.Group>(null!)
+
   return (
     <div
       className="
       flex items-center justify-center flex-col
       h-screen w-full
-      min-h-screen min-w-full bg-zinc-300 font-bold"
+      min-h-screen min-w-full bg-sky-600 font-bold"
     >
       <Canvas shadows gl={{ toneMapping: ACESFilmicToneMapping }}>
-        <PerspectiveCamera makeDefault position={[-2, 5, 13]} />
+        <PerspectiveCamera makeDefault position={[28, 13, 25]} />
+        <OrbitControls enableDamping />
 
-        <directionalLight
-          castShadow
-          shadowMapWidth={1024}
-          shadowMapHeight={1024}
-          shadowCameraFar={50}
-          shadowCameraLeft={-13}
-          shadowCameraRight={13}
-          shadowCameraTop={13}
-          shadowCameraBottom={-13}
-          position={[-2, 10, 5]}
-          intensity={2}
-        />
-        <ambientLight intensity={0.8} />
-        <pointLight intensity={1.5} position={[-10, 0, -20]} />
-        <pointLight intensity={1.5} position={[0, -20, 0]} />
-
-        <group>
+        <group ref={thisGroup}>
+          <ambientLight intensity={0.2} />
           <mesh
             receiveShadow
-            position={[0, -3, 0]}
+            position={[0, -2, 0]}
             rotation={[-Math.PI / 2, 0, 0]}
           >
-            <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            <shadowMaterial attach={'material'} opacity={0.2} />
+            <planeGeometry args={[1000, 1000]} attach={'geometry'} />
+            <meshPhongMaterial
+              attach={'material'}
+              color={'#EB5353'}
+              transparent
+            />
           </mesh>
-    
-          <Box position={[0, 1, 0]} size={[3, 2, 1]} color="#71717a" r={.8} />
-          <Box position={[-2, 1, -5]} color="#52525b" r={0.5} />
-          <Box position={[5, 1, -2]} color="#52525b" r={0.5} />
+          <SpotLight
+            position={[0, 10, 15]}
+            castShadow
+            target={target}
+            penumbra={0.8}
+            radiusTop={0}
+            radiusBottom={50}
+            opacity={0.5}
+            distance={100}
+            angle={0.5}
+            attenuation={20}
+            anglePower={5}
+            intensity={0.4}
+          />
+
+          <Box position={[0, 3, 0]} size={[3, 2, 0.7]} color="#18181b" r={2} />
+          <Box position={[3, 3, -5]} color="#18181b" r={1.5} />
+          <Box position={[-5, 3, -2]} color="#18181b" r={1.5} />
+          <Sky
+            turbidity={0.5}
+            rayleigh={0.8}
+            distance={450000}
+            sunPosition={[0, 0, 100]}
+            inclination={0}
+            azimuth={0.5}
+          />
         </group>
       </Canvas>
     </div>
